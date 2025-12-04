53c041d8df362b75c8d51f2cde4a422ef5e7f7d7
Ivan Vallejo <157355377+IvanDeIvam@users.noreply.github.com>
2025-12-04 18:22:44 +0100

    repte D

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("fred")
+    }
 })
+if (input.temperature() > 22) {
+    basic.showString("calor")
+} else {
+    basic.showString("fred")
+}
