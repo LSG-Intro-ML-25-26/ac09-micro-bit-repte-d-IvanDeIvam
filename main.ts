f81b8ffd7c20d56b6322fcd60e192238a74ea278
Ivan Vallejo <157355377+IvanDeIvam@users.noreply.github.com>
2025-12-10 21:27:55 +0100

    Exercici musica

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
-input.onButtonPressed(Button.A, function () {
+basic.forever(function () {
     if (input.temperature() > 22) {
         basic.showString("calor")
     } else {
         basic.showString("fred")
     }
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("G F G A - F E D ", 120), music.PlaybackMode.InBackground)
+    } else {
+    	
+    }
 })
-if (input.temperature() > 22) {
-    basic.showString("calor")
-} else {
-    basic.showString("fred")
-}
